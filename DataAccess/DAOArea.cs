75bcd469da04fb9a3cd60ce07bc03ab56a9160a4
Omar Orlandini <omarorlandini123@gmail.com>
2017-05-29 22:09:16 -0500

    primer commit de la segunda Version

---
+++ DataAccess/DAOArea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+namespace DataAccess
+{
+    public class DAOArea
+    {
+
+        public Area getArea(int idArea)
+        {
+            List<Area> areas = new List<Area>();
+            Area area = new Area();
+            area.codArea = "1";
+            area.desArea = "Sistemas";
+            areas.Add(area);
+            Area area1 = new Area();
+            area1.codArea = "2";
+            area1.desArea = "Contabilidad";
+            areas.Add(area1);
+
+            foreach (Area a in areas)
+            {
+                if (int.Parse(a.codArea) == idArea) {
+                    return a;
+                }
+            }
+            return null;
+        }
+
+        public List<Area> getAreas(int idSede) {
+            List<Area> listaRpta = new List<Area>();
+            return listaRpta;
+        }
+
+
+    }
+}
---
+++ Entidades/Accesos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public enum Accesos
+    {
+
+        verArchivos,
+        descargarArchivo,
+        subirArchivo,
+        MostrarCrearPerfil,
+        VerPerfilesPanel,
+        VerAccesos,
+        ConfiguracionGeneral,
+    }
+}
